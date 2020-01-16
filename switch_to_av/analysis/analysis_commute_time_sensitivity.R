4952e5a3dac456c47441637aeb7240d7895fc74e
cllorca1 <carlos.llorca@tum.de>
2020-01-16 15:05:13 +0100

    further analyses

--- switch_to_av/analysis/analysis_commute_time_sensitivity.R
+++ switch_to_av/analysis/analysis_commute_time_sensitivity.R
@@ -3,7 +3,7 @@ pacman::p_load(readr, dplyr, data.table, ggplot2, tidyr)
 
 upper_folder = "c:/models/silo/muc/scenOutput/"
 
-scenario = "baseAV2"
+scenario = "baseAV3_nt"
 
 years = c(2011, 2020, 2030, 2040, 2050)
 
@@ -19,12 +19,16 @@ for (year in years){
   workers = nrow(workers_micro_data)
   hh_with_workers = length(unique(workers_micro_data$household))
   
-  hh = workers_micro_data %>% select(household, isAvHh)
+  hh = workers_micro_data %>% select(household, isAvHh, timeCar)
   hh = hh[!duplicated(hh$household),]
   hh_with_avs = nrow(hh %>% filter(isAvHh == "AV"))
   hh_with_cvs = nrow(hh %>% filter(isAvHh == "CV-only"))
-
-  row = data.frame(year, workers, hh_with_workers, hh_with_avs,hh_with_cvs)
+  time_hh_with_cvs = mean((hh %>% filter(isAvHh == "CV-only"))$timeCar)
+  time_hh_with_avs = mean((hh %>% filter(isAvHh == "AV"))$timeCar)
+  time_hh_without_cars = mean((hh %>% filter(isAvHh == "no-car"))$timeCar)
+  
+  
+  row = data.frame(year, workers, hh_with_workers, hh_with_avs,hh_with_cvs,time_hh_with_cvs,time_hh_with_avs,time_hh_without_cars)
   summary = summary %>% bind_rows(row)
 
 
@@ -32,10 +36,16 @@ for (year in years){
 
 summary$hh_without_cars = summary$hh_with_workers - summary$hh_with_avs - summary$hh_with_cvs 
 
-summary2 = summary %>% gather(2:6, key  ="variable", value = "value")
+summary2 = summary %>% gather(c(hh_with_avs, hh_with_cvs, hh_without_cars), key  ="variable", value = "value")
 
 ggplot(summary2, aes(x= year, y = value, color = variable)) + geom_line(size =1) + geom_point(size = 2)
 
 
 ggplot(summary2 %>% filter(variable != "hh_with_workers", variable != "workers"), 
        aes(x= year, y = value, fill = variable)) + geom_bar(stat = "identity", position = "fill")
+
+
+summary3 = summary %>% gather(c(time_hh_with_avs, time_hh_with_cvs, time_hh_without_cars), key  ="variable", value = "value")
+
+ggplot(summary3, aes(x= year, y = value, color = variable)) + geom_line(size =1) + geom_point(size = 2)
+
--- switch_to_av/analysis/modeChoice.R
+++ switch_to_av/analysis/modeChoice.R
@@ -1,4 +1,4 @@
-pacman::p_load(readr, dplyr, data.table, ggplot2, tidyr, rhdf5)
+pacman::p_load(readr, dplyr, data.table, ggplot2, tidyr, rhdf5, sf)
 
 
 upper_folder = "c:/models/silo/muc/scenOutput/"
@@ -16,6 +16,8 @@ ggplot(modeChoice, aes(x=year, fill = mode)) + geom_bar(stat = "count", position
 
 ggplot(modeChoice, aes(x=year, fill = mode)) + geom_bar(stat = "count", position = "stack") + 
   facet_wrap(.~zeroAutoHh)
+
+ggplot(modeChoice, aes(x=year, fill = mode)) + geom_bar(stat = "count", position = "stack")
 
 
 ggplot(modeChoice, aes(x=year, fill = mode)) + geom_bar(stat = "count", position = "fill") + 
@@ -35,5 +37,55 @@ matrix = readMatrixOMX(path_to_matrix, "distanceByTime")
 
 modeChoice = modeChoice %>% rowwise() %>% mutate(distance = matrix[homeZone, workZone])
 
-ggplot(modeChoice %>% filter(year == 2011), aes(x=distance, fill = mode)) + geom_area(stat = "bin", position = "fill")
-ggplot(modeChoice %>% filter(year == 2049), aes(x=distance, fill = mode)) + geom_area(stat = "bin", position = "fill")
+selected_years = c(2011,2030,2049)
+
+ggplot(modeChoice %>% filter(year %in% selected_years), aes(x=distance, fill = mode)) +
+  geom_area(stat = "bin", position = "stack") + facet_grid(zeroAutoHh~year, scales = "free_y")
+
+
+
+
+
+zones = st_read("c:/models/silo/muc/input/zonesShapefile/zones.shp")
+
+modeChoiceAtJob  =modeChoice %>% group_by(year, workZone, mode) %>% summarize(count = n()) %>% spread(mode, count, fill = 0)
+modeChoiceAtJob$total = modeChoiceAtJob$av + modeChoiceAtJob$car + modeChoiceAtJob$pt
+modeChoiceAtJob$shareAv = modeChoiceAtJob$av / modeChoiceAtJob$total
+modeChoiceAtJob$sharePt = modeChoiceAtJob$pt / modeChoiceAtJob$total
+modeChoiceAtJob$shareCv = modeChoiceAtJob$car / modeChoiceAtJob$total
+
+zones = zones %>% left_join(modeChoiceAtJob %>% filter(year == 2011), by = c("id" = "workZone"))
+zones [is.na(zones)] = 0 #fills zones without observation of mode choice
+
+regions = zones %>% group_by(AGS) %>% summarize(shareAv = weighted.mean(shareAv, total), 
+                                                shareCv = weighted.mean(shareCv, total),
+                                                sharePt = weighted.mean(sharePt, total))
+
+p  = tm_basemap(leaflet::providers$CartoDB) +
+  tm_shape(regions, name = "Av") +  tm_polygons(col = "shareAv") + 
+  tm_shape(regions, name = "Pt")  + tm_polygons(col = "sharePt") + 
+  tm_shape(regions, name = "Cv") + tm_polygons(col = "shareCv")
+
+tmap_leaflet(p)
+
+
+zoneTypes = read_csv("c:/models/silo/muc/input/zoneSystem.csv")
+
+zoneTypes = zoneTypes %>% select(id = Zone, area = Area, type = BBSR_Type)
+
+shareByZoneType = zoneTypes %>% left_join(modeChoiceAtJob , by = c("id" = "workZone"))
+shareByZoneType [is.na(shareByZoneType)] = 0
+
+
+
+shareByZoneType = shareByZoneType %>% group_by(type, year) %>% summarize(shareAv = weighted.mean(shareAv, total), 
+                                                         shareCv = weighted.mean(shareCv, total),
+                                                         sharePt = weighted.mean(sharePt, total))
+
+shareByZoneType = shareByZoneType %>% gather(3:5, key = "mode", value = "share")
+
+shareByZoneType$typeWord = factor(x = shareByZoneType$type, levels = c(10,20,30,40), labels = c("Core", "Medium city", "Town", "Rural"))
+
+ggplot(shareByZoneType %>% filter(year %in% selected_years), aes(x = typeWord, fill = mode, y = share)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(.~year)
